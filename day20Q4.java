9ce22831b7e2daaf0bbcf6a48e317d742dee7b1a
Shivansh Singh <thakurshivanshsingh20@gmail.com>
2026-06-16 19:52:10 +0530

    Update day20Q4.java

--- day20Q4.java
+++ day20Q4.java
@@ -6,9 +6,6 @@ public class day20Q4 {
         System.out.println("Enter number of rows and colom of matrix : ");
         int m= sc.nextInt();
         int n=sc.nextInt();
-        if(n!=m){
-            System.out.println("it is not a square ,hence not symmetric ");
-        }
        
         //initialise matrix 
         int[][] mat=new int[m][n];
